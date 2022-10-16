51aa36ab6edd4a77ff82995c382e09f004ac7148
Oleg <marchello3007@gmail.com>
2022-10-16 18:43:56 +0300

    add HederRow & main menu

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -4,26 +4,12 @@ import Header from 'container/Header/Header'
 import Main from 'container/Main/Main'
 import Footer from 'container/Footer/Footer'
 
-type Props88 = {
-    title: string
-}
-
-const AppHeader = (props: Props88) => {
-    return <h1>Hello{props.title}</h1>
-}
-
-const AppList = () => {
-    return <div>Bla Bla Bla</div>
-}
+type Props = {}
 
 const App = () => {
     return (
         <>
             <CssBaseline />
-            <AppHeader title="Yo" />
-            <AppHeader title="Yo Yo" />
-            <AppList />
-
             <Header />
             <Main />
             <Footer />
--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import HeaderRow from 'components/HeaderRow/HeaderRow'
 
 type Props = {}
 
 const Header = (props: Props) => {
-    return <div>Header</div>
+    return (
+        <>
+            <HeaderRow />
+        </>
+    )
 }
 
 export default Header
